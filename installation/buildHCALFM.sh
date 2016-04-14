b40f74dc948de59dc631e7eda4fe2cf7c55aabc7
jhakala <john_hakala@brown.edu>
2016-04-14 15:23:55 +0200

    beef up versioning script

--- installation/buildHCALFM.sh
+++ installation/buildHCALFM.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
-GITREV=`git rev-parse HEAD | head -c 7`
-ant -DgitRev="${GITREV}"
+# Script to keep the HCALFM builds organized
+# Production HCALFMs must be built from an unmodified git commit, and are indexed by their commit hash.
+# Test HCALFMs are organized by date, appended with a version number.
+# Usage: ./buildHCALFM.sh test
+# The first option can be either "test" or "release".
+# John Hakala 4/14/2016
+
+if [ "$1" = "release" ]; then
+  ANYCHANGES=`git diff-index --quiet HEAD`
+  if [ "$ANYCHANGES" = "0" ]; then
+		GITREV=`git rev-parse HEAD | head -c 7`
+		ant -DgitRev="${GITREV}"
+  else
+    echo "No changes since the last commit are permitted when building a release FM. Please commit your changes or stash them."
+    exit 1
+  fi
+    
+elif [ "$1" = "test" ]; then
+	DATE=`date  +%m-%d-%y`
+  ITERATION=1
+  
+  while [ -f jars/HCALFM_${DATE}_v${ITERATION}.jar ];
+    do ITERATION=$(($ITERATION + 1))
+  done
+  ant -DgitRev="${DATE}_v${ITERATION}"
+
+else 
+	echo "Please run buildHCALFM with either the 'release' or 'test' option. Example:\n./buildHCALFM.sh test"
+  exit 1
+
+fi
+
