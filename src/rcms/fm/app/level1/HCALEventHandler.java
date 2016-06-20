ebf7e3299f40e14c467c68bb7b397ac345cb255f
kakwok <ka_hei_martin_kwok@brown.edu>
2016-06-20 11:38:13 +0200

    Get CommonMS from MS used

--- src/rcms/fm/app/level1/HCALEventHandler.java
+++ src/rcms/fm/app/level1/HCALEventHandler.java
@@ -605,7 +605,8 @@ public class HCALEventHandler extends UserEventHandler {
 
     // Try to find a common masterSnippet from UserXML
     try{
-			CommonMasterSnippetFile = xmlHandler.getNamedUserXMLelementAttributeValue("Common","MasterSnippet","snippet");
+			//CommonMasterSnippetFile = xmlHandler.getNamedUserXMLelementAttributeValue("Common","MasterSnippet","snippet");
+			CommonMasterSnippetFile = xmlHandler.getHCALMasterSnippetTag(selectedRun,CfgCVSBasPath,"CommonMS");
 
 			if (CommonMasterSnippetFile.equals("")){
 			    logger.info("[Martin log " + functionManager.FMname + "] No CommonMasterSnippet was found. Only the <CfgScript> content in MasterSnippet will be used.");
