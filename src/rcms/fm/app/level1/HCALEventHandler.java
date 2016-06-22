8157f32899474ed43a87d2a1d676647af6c1a3d1
kakwok <ka_hei_martin_kwok@brown.edu>
2016-06-22 11:55:04 +0200

    Put the filename of CommonMS in attribute

--- src/rcms/fm/app/level1/HCALEventHandler.java
+++ src/rcms/fm/app/level1/HCALEventHandler.java
@@ -606,7 +606,11 @@ public class HCALEventHandler extends UserEventHandler {
     // Try to find a common masterSnippet from UserXML
     try{
 			//CommonMasterSnippetFile = xmlHandler.getNamedUserXMLelementAttributeValue("Common","MasterSnippet","snippet");
-			CommonMasterSnippetFile = xmlHandler.getHCALMasterSnippetTag(selectedRun,CfgCVSBasePath,"CommonMS");
+			//CommonMasterSnippetFile = xmlHandler.getHCALMasterSnippetTag(selectedRun,CfgCVSBasePath,"CommonMS");
+			//
+			String TagName="CommonMasterSnippet";
+      String attribute="file";
+			CommonMasterSnippetFile = xmlHandler.getHCALMasterSnippetTag(selectedRun,CfgCVSBasePath,TagName,attribute);
 
 			if (CommonMasterSnippetFile.equals("")){
 			    logger.info("[Martin log " + functionManager.FMname + "] No CommonMasterSnippet was found. Only the <CfgScript> content in MasterSnippet will be used.");
