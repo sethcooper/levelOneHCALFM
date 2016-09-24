c931749fbca695ef14ca7f8415efc66a28ee7f03
kakwok <ka_hei_martin_kwok@brown.edu>
2016-09-24 16:51:28 +0200

    Remove L2Priority1,2 containers

--- src/rcms/fm/app/level1/HCALFunctionManager.java
+++ src/rcms/fm/app/level1/HCALFunctionManager.java
@@ -130,9 +130,6 @@ public class HCALFunctionManager extends UserFunctionManager {
   // container with all FunctionManagers except the EvmTrig and TCDSLPM
   public QualifiedResourceContainer containerFMChildrenNoEvmTrigNoTCDSLPM = null;
 
-  public QualifiedResourceContainer containerFMChildrenL2Priority1 = null;
-  public QualifiedResourceContainer containerFMChildrenL2Priority2 = null;
-  public QualifiedResourceContainer containerFMChildrenL2Laser = null;
   public QualifiedResourceContainer containerFMChildrenEvmTrig = null;
   public QualifiedResourceContainer containerFMChildrenNormal = null;
 
